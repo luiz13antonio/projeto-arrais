1d1c2e9bcbd9ff17957922f4923566e5bb965ee2
luiz13antonio <luizantonio.ce2012@gmail.com>
2025-01-27 10:23:14 -0300

    a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
+import { Button } from "antd";
 const Home = () => {
     return (
+    <>
+
         <div className="w-full h-[50px] flex justify-end items-center">
-            <button>Login</button>
+            <Button
+               type="primary" 
+               size="large" 
+               className="bg-laranja  hover: !bg-laranja2"
+            >
+            Login        
+            </Button>
+
+                
+            </section></>
         </div>
     );
+    </>
+    
 }
- 
+
 export default Home;
